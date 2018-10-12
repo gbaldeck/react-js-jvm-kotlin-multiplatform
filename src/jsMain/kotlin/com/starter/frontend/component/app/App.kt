9bc03273e74653b2e4d9a646aad24804a6af6e67
Graham Baldeck <gbaldeck@gmail.com>
2018-10-12 12:43:03 -0700

    Update App.kt

--- src/jsMain/kotlin/com/starter/frontend/component/app/App.kt
+++ src/jsMain/kotlin/com/starter/frontend/component/app/App.kt
@@ -8,7 +8,9 @@ import react.dom.div
 import kotlinext.js.require
 
 private class App: RComponent<RProps, RState>() {
-    init { require("Components/app/App.scss") }
+    //tells webpack to load the SASS file as a module and uses the `Components` alias we setup in 
+    //webpack.common.js so we don't have to type out the whole path to the file
+    init { require("Components/app/App.scss") } 
 
     override fun RBuilder.render() {
         div(classes = "bm-app") {
